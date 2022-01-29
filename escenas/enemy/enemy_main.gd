2779cd5f663119b20965962341844fb7e3a66c03
MigueStarchaser <x43090@hotmail.com>
2022-01-28 23:38:12 -0600

    Enemies
    Enemies

--- escenas/enemy/enemy_main.gd
+++ escenas/enemy/enemy_main.gd
@@ -13,18 +13,27 @@ var _velocity 			= Vector2();
 var hp 		= 3;
 var type	= 0;
 var status 	= 0;	
+var spt 	= 0;
+var _size 	= Vector2();
+var _scale 	= Vector2();	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$scale.set_wait_time(0.1);
+	$scale.start();
+	$scale.paused = true;
+	$scale.connect("timeout", self, "_resize")
 	randomize();
 	var random_value 	= rand_range(0,2);
 	random_value		= int(floor(random_value));
 	#print("spawn:"+str(random_value));
 	if(random_value == 0):
+		spt = 0;
 		$ghost.show();
 		$ghost.play();
 		random_value 	= rand_range(0,2);
 		random_value		= int(floor(random_value));
+		_scale = $ghost.scale;
 		if(random_value == 0):
 			$ghost.animation = "luz";
 			type = 0;
@@ -32,8 +41,10 @@ func _ready():
 			$ghost.animation = "dark";
 			type = 1;
 	elif(random_value == 1):
+		spt = 1;
 		$skeleton.show();
 		$skeleton.play();
+		_scale = $skeleton.scale;
 		random_value 	= rand_range(0,2);
 		random_value		= int(floor(random_value));
 		if(random_value == 0):
@@ -94,4 +105,27 @@ func _demage(_type_bullet):
 		hp = hp -1;
 		if(hp == 0):
 			queue_free();
+		else:
+			var s = Vector2();
+			if(spt == 0):
+				s.x = $ghost.scale.x + 0.070 ;
+				s.y = $ghost.scale.y + 0.070 ;
+				$ghost.scale = s;
+			else:
+				s.x = $skeleton.scale.x + 0.070 ;
+				s.y = $skeleton.scale.y + 0.070 ;
+				$skeleton.scale = s;
+			$scale.start();
+			$scale.paused = false;
 	pass	
+
+func _resize():
+	var s = Vector2();
+	s.x = _scale.x ;
+	s.y = _scale.y ;
+	if(spt == 0):
+		$ghost.scale = s;
+	else:
+		$skeleton.scale = s;
+	$scale.paused = true;
+	pass
